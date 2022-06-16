8b2f31fdd4ca63a83ede3ba5adcb16659050fada
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2022-06-16 15:01:08 +0700

    Add files via upload

---
+++ PdfLightTableSyncusion/PdfLightTableSyncusion/frmPdfLightTable.designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPdfLightTable
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim MessageBoxSettings2 As Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings = New Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings()
+        Dim PdfViewerPrinterSettings2 As Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings = New Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPdfLightTable))
+        Dim TextSearchSettings2 As Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings = New Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings()
+        Me.PdfViewerControl1 = New Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl()
+        Me.SuspendLayout()
+        '
+        'PdfViewerControl1
+        '
+        Me.PdfViewerControl1.CursorMode = Syncfusion.Windows.Forms.PdfViewer.PdfViewerCursorMode.SelectTool
+        Me.PdfViewerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PdfViewerControl1.EnableContextMenu = True
+        Me.PdfViewerControl1.EnableNotificationBar = True
+        Me.PdfViewerControl1.HorizontalScrollOffset = 0
+        Me.PdfViewerControl1.IsBookmarkEnabled = True
+        Me.PdfViewerControl1.IsTextSearchEnabled = True
+        Me.PdfViewerControl1.IsTextSelectionEnabled = True
+        Me.PdfViewerControl1.Location = New System.Drawing.Point(0, 0)
+        MessageBoxSettings2.EnableNotification = True
+        Me.PdfViewerControl1.MessageBoxSettings = MessageBoxSettings2
+        Me.PdfViewerControl1.MinimumZoomPercentage = 50
+        Me.PdfViewerControl1.Name = "PdfViewerControl1"
+        Me.PdfViewerControl1.PageBorderThickness = 1
+        PdfViewerPrinterSettings2.PageOrientation = Syncfusion.Windows.PdfViewer.PdfViewerPrintOrientation.[Auto]
+        PdfViewerPrinterSettings2.PageSize = Syncfusion.Windows.PdfViewer.PdfViewerPrintSize.ActualSize
+        PdfViewerPrinterSettings2.PrintLocation = CType(resources.GetObject("PdfViewerPrinterSettings2.PrintLocation"), System.Drawing.PointF)
+        PdfViewerPrinterSettings2.ShowPrintStatusDialog = True
+        Me.PdfViewerControl1.PrinterSettings = PdfViewerPrinterSettings2
+        Me.PdfViewerControl1.ReferencePath = Nothing
+        Me.PdfViewerControl1.ScrollDisplacementValue = 0
+        Me.PdfViewerControl1.ShowHorizontalScrollBar = True
+        Me.PdfViewerControl1.ShowToolBar = True
+        Me.PdfViewerControl1.ShowVerticalScrollBar = True
+        Me.PdfViewerControl1.Size = New System.Drawing.Size(1015, 647)
+        Me.PdfViewerControl1.SpaceBetweenPages = 8
+        Me.PdfViewerControl1.TabIndex = 1
+        Me.PdfViewerControl1.Text = "PdfViewerControl1"
+        TextSearchSettings2.CurrentInstanceColor = System.Drawing.Color.FromArgb(CType(CType(127, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(171, Byte), Integer), CType(CType(64, Byte), Integer))
+        TextSearchSettings2.HighlightAllInstance = True
+        TextSearchSettings2.OtherInstanceColor = System.Drawing.Color.FromArgb(CType(CType(127, Byte), Integer), CType(CType(254, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PdfViewerControl1.TextSearchSettings = TextSearchSettings2
+        Me.PdfViewerControl1.ThemeName = "Default"
+        Me.PdfViewerControl1.VerticalScrollOffset = 0
+        Me.PdfViewerControl1.VisualStyle = Syncfusion.Windows.Forms.PdfViewer.VisualStyle.[Default]
+        Me.PdfViewerControl1.ZoomMode = Syncfusion.Windows.Forms.PdfViewer.ZoomMode.[Default]
+        '
+        'frmSyncfusionPDF
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1015, 647)
+        Me.Controls.Add(Me.PdfViewerControl1)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "frmSyncfusionPDF"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PdfLightTable Syncfusion - coDe bY: Thongkorn Tubtimkrob"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PdfViewerControl1 As Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl
+
+End Class
---
+++ PdfLightTableSyncusion/PdfLightTableSyncusion/frmPdfLightTable.vb
@@ -0,0 +1,179 @@
+﻿#Region "About"
+' / ---------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com/webboard
+' /
+' / Purpose: Create/Generate PDF from DataTable with Syncfusion PDF .NET Library.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / ---------------------------------------------------------------
+#End Region
+
+Imports Syncfusion.Pdf
+Imports Syncfusion.Windows.Forms
+Imports Syncfusion.Pdf.Parsing
+Imports Syncfusion.Windows.Forms.Grid
+Imports Syncfusion.Pdf.Graphics
+Imports Syncfusion.Pdf.Tables
+
+'/ H E L P
+'/ https://help.syncfusion.com/cr/file-formats/Syncfusion.Pdf.Tables.PdfLightTable.html
+
+Public Class frmPdfLightTable
+    '/ Customize the application path.
+    Dim strPathPDF As String = MyPath(Application.StartupPath)
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    ' / AppPath = C:\My Project\bin\debug
+    ' / Replace "\bin\debug" with "\"
+    ' / Return : C:\My Project\
+    Function MyPath(ByVal AppPath As String) As String
+        '/ Return Value
+        MyPath = AppPath.ToLower.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\")
+        '// Put the \ (BackSlash) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+    '/ START HERE
+    Private Sub frmPdfLightTable_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Call SamplePDF()
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Create or Generate PDF with Syncfusion.
+    Private Sub SamplePDF()
+        '/ Create a new PDF Document.
+        Dim Doc As New PdfDocument()
+        Doc.PageSettings.Size = PdfPageSize.A4
+        Doc.PageSettings.Orientation = PdfPageOrientation.Portrait
+        '/ Size A5 and Landscape
+        'Doc.PageSettings.Size = PdfPageSize.A5
+        'Doc.PageSettings.Orientation = PdfPageOrientation.Landscape
+        '/ Add a Page.
+        Dim Page As PdfPage = Doc.Pages.Add()
+
+        '/ Create a PdfLightTable  
+        Dim PdfLightTable As New PdfLightTable
+        Try
+            '/ Assign data source.
+            PdfLightTable.DataSource = GetDataTable()
+            '/ Set Column Headers of PdfLightTable
+            With PdfLightTable
+                .Columns(0).ColumnName = "Primary Key"
+                .Columns(1).ColumnName = "รหัสสินค้า"
+                .Columns(2).ColumnName = "ชื่อสินค้า"
+                .Columns(3).ColumnName = "ราคา"
+            End With
+            '/ Create PDF graphics for the page
+            Dim logo As PdfGraphics = Page.Graphics
+            '/ Load the image from the disk.
+            Dim image As New PdfBitmap(strPathPDF & "g2gnet.png")
+            '/ Draw the image.
+            logo.DrawImage(image, 10, 0)
+            '/ Create Text Header.
+            Dim HeaderFont As PdfFont = New PdfTrueTypeFont(New Font("Century Gothic", 18, FontStyle.Bold), True)
+            '/ Create PDF graphics for the page.
+            Dim graphics As PdfGraphics = Page.Graphics
+            '/ Draw the text.
+            graphics.DrawString("PDF Light Table Syncfusion (PDF .NET Library)", HeaderFont, PdfBrushes.Black, New PointF(100, 10))
+            '/ Set new Font.
+            HeaderFont = New PdfTrueTypeFont(New Font("Arial Unicode MS", 20, FontStyle.Bold), True)    '/ True = Unicode
+            graphics.DrawString("ทดสอบภาษาไทย ประถมศึกษาชั้นปีที่ 1", HeaderFont, PdfBrushes.Black, New PointF(130, 35))
+
+            '/ Declare font and define the header style in the table.
+            Dim Font As PdfFont = New PdfTrueTypeFont(New Font("Arial Unicode MS", 12, FontStyle.Regular), True)
+            Dim MyStyle = New PdfCellStyle(Font, PdfBrushes.Black, PdfPens.Orange)
+            Dim HeaderStyle As New PdfCellStyle(Font, PdfBrushes.White, PdfPens.Orange)
+            HeaderStyle.BackgroundBrush = PdfBrushes.Orange
+            With PdfLightTable
+                .Style.HeaderStyle = HeaderStyle
+                .Style.DefaultStyle = MyStyle
+                '/ Set cell padding, Spacing.
+                .Style.CellPadding = 5
+                .Style.CellSpacing = 0
+            End With
+
+            '/ Create a new string format.
+            Dim Format As New PdfStringFormat
+            For i = 0 To 2
+                '/ Set the text Alignment columns 0- 2.
+                Format.Alignment = PdfTextAlignment.Left
+                Format.LineAlignment = PdfVerticalAlignment.Middle
+                PdfLightTable.Columns(i).StringFormat = Format
+            Next
+            '/ Set new stringformat.
+            Format = New PdfStringFormat
+            With Format
+                .Alignment = PdfTextAlignment.Right
+                .LineAlignment = PdfVerticalAlignment.Middle
+            End With
+            '/ Add the string format to PdfLightTable column.
+            PdfLightTable.Columns(3).StringFormat = Format
+
+            '/ Show header in the table.
+            PdfLightTable.Style.ShowHeader = True
+            '/ Draw grid to the Page of PDF Document.
+            PdfLightTable.Draw(Page, New PointF(10, 76))
+
+            '/ Save the Document
+            Doc.Save(strPathPDF & "Output.pdf")
+            '/ Close the Document
+            Doc.Close(True)
+            '/ Open PDF on PDFViewerControl
+            Me.PdfViewerControl1.Load(strPathPDF & "Output.pdf", "")    '/ "" No Password
+
+        Catch ex As Exception
+            MessageBoxAdv.MessageBoxStyle = MessageBoxAdv.Style.Metro
+            MessageBoxAdv.MessageFont = New Font("Tahoma", 12, FontStyle.Regular)
+            MessageBoxAdv.Show(ex.Message, "รายงานความผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    '// SAMPLE DATATABLE
+    Private Function GetDataTable() As DataTable
+        Dim DT As New DataTable
+        '// Add Columns
+        With DT.Columns
+            .Add("PK", GetType(Integer))
+            .Add("ProductID", GetType(String))
+            .Add("ProductName", GetType(String))
+            .Add("UnitPrice", GetType(String))
+        End With
+        '// Add Rows
+        With DT.Rows
+            '// PK, ProductID, ProductName, UnitPrice
+            .Add(1, "01", "Classic Chicken", "45.00")
+            .Add(2, "02", "Mexicana", "85.00")
+            .Add(3, "03", "Lemon Shrimp", "110.00")
+            .Add(4, "04", "Bacon", "90.00")
+            .Add(5, "05", "Spicy Shrimp", "120.00")
+            .Add(6, "06", "Tex Supreme", "80.00")
+            .Add(7, "07", "Fish", "100.00")
+            .Add(8, "08", "Pepsi Small", "20.00")
+            .Add(9, "09", "Coke Small", "20.00")
+            .Add(10, "10", "7Up Small", "20.00")
+            .Add(11, "11", "มอคค่า", "85.00")
+            .Add(12, "12", "อเมริกาโน่", "95.00")
+            .Add(13, "13", "เอ็กซ์เพรสโซ่", "115.00")
+            .Add(14, "14", "ชานมไข่มุก", "45.00")
+            .Add(15, "15", "เหล้าเป็ก", "99.00")
+        End With
+        Return DT
+    End Function
+
+    Private Sub frmPdfLightTable_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+End Class
